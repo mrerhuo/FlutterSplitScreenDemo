3119379bc3a7f94fa8eca02dd308c8ed5a8bd268
mrer <silence_ailong@qq.com>
2019-02-26 22:48:45 +0800

    修改了显示页面顺序

--- lib/CarClass/ListWidget.dart
+++ lib/CarClass/ListWidget.dart
@@ -5,7 +5,8 @@ import 'Car.dart';
 typedef Null ItemSelectedCallback(Car value);
 
 class ListWidget extends StatefulWidget{
-  ListWidget(this.title,this.onItemSelected);
+  //ListWidget(this.title,this.onItemSelected);
+  ListWidget({Key key, this.title,this.onItemSelected}) : super(key: key);
   final String title;
   final ItemSelectedCallback onItemSelected;
   @override
--- lib/SplitScreen/SplitScreenState.dart
+++ lib/SplitScreen/SplitScreenState.dart
@@ -4,18 +4,15 @@ import '../CarClass/ListWidget.dart';
 import '../CarClass/DetailWidget.dart';
 import '../CarClass/OtherDetailWidget.dart';
 import '../CarClass/Car.dart';
-class SplitScreenState extends State<SplitScreenWidget>{
 
+class SplitScreenState extends State<SplitScreenWidget> {
   Car selectedValue;
   var isLargeScreen = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: new Text("Split Screen Demo"),
-        centerTitle:true
-      ),
+      appBar: AppBar(title: new Text("Split Screen Demo"), centerTitle: true),
       body: OrientationBuilder(builder: (context, orientation) {
         //判断是否分屏，根据宽度决定大于600为true,需要分屏
         if (MediaQuery.of(context).size.width > 600) {
@@ -24,22 +21,43 @@ class SplitScreenState extends State<SplitScreenWidget>{
           isLargeScreen = false;
         }
         //定义Row
+        // return Row(children: <Widget>[
+        //   Expanded(
+        //     child: DetailWidget("列表",(value) {
+        //       if (isLargeScreen) {
+        //         selectedValue = value;
+        //         setState(() {});
+        //       } else {
+        //         Navigator.push(context, MaterialPageRoute(
+        //           builder: (context) {
+        //             return OtherDetailWidget(car:value);
+        //           },
+        //         ));
+        //       }
+        //     }),
+        //   ),
+        //   isLargeScreen ? Expanded(child: DetailWidget(car:selectedValue)) : Container(),
+        // ]);
         return Row(children: <Widget>[
-          Expanded(
-            child: ListWidget("列表",(value) {
-              if (isLargeScreen) {
-                selectedValue = value;
-                setState(() {});
-              } else {
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (context) {
-                    return OtherDetailWidget(car:value);
-                  },
-                ));
-              }
-            }),
-          ),
-          isLargeScreen ? Expanded(child: DetailWidget(car:selectedValue)) : Container(),
+          isLargeScreen
+              ? Expanded(
+                  child: ListWidget(
+                      title: "fff",
+                      onItemSelected: (value) {
+                        if (isLargeScreen) {
+                          selectedValue = value;
+                          setState(() {});
+                        } else {
+                          Navigator.push(context, MaterialPageRoute(
+                            builder: (context) {
+                              return OtherDetailWidget(car: value);
+                            },
+                          ));
+                        }
+                      }),
+                  flex: 1)
+              : Container(),
+          Expanded(child: DetailWidget(car: selectedValue), flex: 2),
         ]);
       }),
     );
